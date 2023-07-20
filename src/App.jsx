2be7217b883e217e4b4b3878ace15c1973a7f117
viacheslav <viacheslav.saiapin@gmail.com>
2023-07-20 20:28:15 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ChessFigureService } from "./services/СhessFigureService";
+
+const App = () => {
+  const board = new ChessFigureService();
+
+
+
+
+  return <div></div>;
+};
+
+export default App;
